dd9ec6328c9e32a9ada6656b6dcaff3c00d0bbca
Matt Eck <ME.MattEck@gmail.com>
2019-10-06 11:30:32 -0400

    added linter templates established

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World'
+    user = {'username': 'Matt'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'This is a post'
+        },
+        {
+            'author': {'username': 'Kelsey'},
+            'body': 'Posting a thing'
+        }
+    ]
+    return render_template('index.html', title = 'Home', user = user, posts = posts)
